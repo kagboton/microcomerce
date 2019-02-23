748fbba4b16da87bb1780e12b263d8d865c6621f
Kilian Agboton <kilian.agboton@gmail.com>
2019-02-24 00:43:50 +0100

    creation du microservice mclientui

---
+++ microservice-clientui/src/main/java/com/mclientui/mclientui/MclientuiApplication.java
@@ -0,0 +1,13 @@
+package com.mclientui.mclientui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MclientuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MclientuiApplication.class, args);
+	}
+
+}
---
+++ microservice-clientui/src/test/java/com/mclientui/mclientui/MclientuiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mclientui.mclientui;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MclientuiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
