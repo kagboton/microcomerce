25831217110979d38740402354e05812e4584395
Kilian Agboton <kilian.agboton@gmail.com>
2019-02-26 00:41:31 +0100

    afficher detail produit

--- microservice-clientui/src/main/java/com/mclientui/web/controller/ClientController.java
+++ microservice-clientui/src/main/java/com/mclientui/web/controller/ClientController.java
@@ -6,6 +6,7 @@ import com.mclientui.proxy.MicroserviceProduitsProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -22,5 +23,14 @@ public class ClientController {
         List<ProductBean> produits = mProduitsProxy.listeDesProduits();
         model.addAttribute("produits", produits);
         return "Accueil";
+    }
+
+    @RequestMapping("/details-produit/{id}")
+    public String detailProduit(@PathVariable int id, Model model){
+        ProductBean produit = mProduitsProxy.recupererUnProduit(id);
+        model.addAttribute("produit", produit);
+
+        return "FicheProduit";
+
     }
 }
