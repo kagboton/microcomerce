4eb690c832e638585472b0e53356874823bfe72a
Kilian Agboton <kilian.agboton@gmail.com>
2019-02-26 19:42:38 +0100

    config d'une propriété limitProduits

---
+++ microservice-produits/src/main/java/com/mproduits/configuration/ApplicationPropertiesConfig.java
@@ -0,0 +1,20 @@
+package com.mproduits.configuration;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("mes-configs")
+public class ApplicationPropertiesConfig {
+
+    private int limiteDeProduits;
+
+    public int getLimiteDeProduits() {
+        return limiteDeProduits;
+    }
+
+    public void setLimiteDeProduits(int limiteDeProduits) {
+        this.limiteDeProduits = limiteDeProduits;
+    }
+}
