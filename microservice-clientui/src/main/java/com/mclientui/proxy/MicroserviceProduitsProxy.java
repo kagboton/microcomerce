cf3914be723571a37e5761707dbac846b6e47f4f
Kilian Agboton <kilian.agboton@gmail.com>
2019-03-01 20:52:09 +0100

    connexion de clientui à zuul

--- microservice-clientui/src/main/java/com/mclientui/proxy/MicroserviceProduitsProxy.java
+++ microservice-clientui/src/main/java/com/mclientui/proxy/MicroserviceProduitsProxy.java
@@ -8,13 +8,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "microservice-produits", decode404 = true)
+@FeignClient(name = "zuul-server", decode404 = true)
 @RibbonClient(name = "microservice-produits")
 public interface MicroserviceProduitsProxy {
 
-    @GetMapping(value = "/Produits")
+    @GetMapping(value = "/microservice-produits/Produits")
     List<ProductBean> listeDesProduits();
 
-    @GetMapping( value = "/Produits/{id}")
+    @GetMapping( value = "/microservice-produits/Produits/{id}")
     ProductBean recupererUnProduit(@PathVariable("id") int id);
 }
